86d53e4eb90c24e297aa90583ec885020914bb5c
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-12-03 23:23:49 +0000

    chore: fix type errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
+
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
